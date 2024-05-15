adfa255d00d096ae3df713d64e5b54b31048e89c
EC2 Default User <ec2-user@ip-172-31-7-132.ap-northeast-1.compute.internal>
2024-05-15 08:40:50 +0000

    Add Book.count

---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+	
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+  
+  scope :created_today,->{where(created_at: Time.zone.now.all_day)}
+    scope :created_yesterday,->{where(created_at: 1.day.ago.all_day)}
+   
+    
+end
